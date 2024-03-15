979e04a0bb2d746ba1ebb4c4d0d0753ad2415647
melch <clement.passet@gmail.com>
2024-03-15 10:11:51 +0100

    chore: initial commit

---
+++ src/main/java/com/cpst/apichatop/ApichatopApplication.java
@@ -0,0 +1,13 @@
+package com.cpst.apichatop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApichatopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApichatopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cpst/apichatop/ApichatopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cpst.apichatop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApichatopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
