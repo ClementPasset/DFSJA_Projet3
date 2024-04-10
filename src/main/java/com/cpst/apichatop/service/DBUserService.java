f4fe5416f640c815f9f6fba541f99e677ae0121f
melch <clement.passet@gmail.com>
2024-04-10 23:08:26 +0200

    adding DBUserService

--- src/main/java/com/cpst/apichatop/service/DBUserService.java
+++ src/main/java/com/cpst/apichatop/service/DBUserService.java
@@ -1,8 +1,12 @@
 package com.cpst.apichatop.service;
 
+import java.security.Principal;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.stereotype.Service;
 
 import com.cpst.apichatop.model.DBUser;
@@ -21,5 +25,34 @@ public class DBUserService {
     public DBUser saveDBUser(DBUser user){
         DBUser newUser = dbUserRepository.save(user);
         return newUser;
+    }
+
+    public String getEmailFromAuthentication(Principal user) {
+        String email = "";
+        if (user instanceof UsernamePasswordAuthenticationToken) {
+            email = getemailFromUsernamePasswordAuthentication(user);
+        } else if (user instanceof JwtAuthenticationToken) {
+            email = getemailFromJwtAuthentication(user);
+        }
+        return email;
+    }
+
+    private String getemailFromUsernamePasswordAuthentication(Principal user) {
+        UsernamePasswordAuthenticationToken token = (UsernamePasswordAuthenticationToken) user;
+        if (token.isAuthenticated()) {
+            User u = (User) token.getPrincipal();
+            return u.getUsername();
+        } else {
+            return null;
+        }
+    }
+
+    private String getemailFromJwtAuthentication(Principal user) {
+        JwtAuthenticationToken token = (JwtAuthenticationToken) user;
+        if (token.isAuthenticated()) {
+            return token.getTokenAttributes().get("sub").toString();
+        } else {
+            return null;
+        }
     }
 }
